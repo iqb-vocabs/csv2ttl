5d8dd3f92869fb125b41f147b3b6ea2d47ec0d43
Yan Ping Chen Lin <yanpingchenlin@gmail.com>
2023-09-05 12:29:16 +0200

    first working version

--- src/csv2ttl.ts
+++ src/csv2ttl.ts
@@ -1,8 +1,6 @@
 import { parse as csv_parse} from 'csv-parse/sync';
 import {Namespace, graph, literal} from "rdflib";
 
-// import { graph, Literal, Namespace } from 'rdflib';
-
 let data_folder = '.';
 if (process.argv[2]) {
     data_folder = `${data_folder}/${process.argv[2]}`;
@@ -12,8 +10,7 @@ const config_filename = `${data_folder}/csv2ttl_config.json`;
 const fs = require('fs');
 const rdflib = require('rdflib');
 
-
-const DCTERMS = Namespace("http://purl.org/dc/terms/#");
+const DCTERMS = Namespace("http://purl.org/dc/terms/")
 const RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
 const SKOS = Namespace("http://www.w3.org/2004/02/skos/core#");
 
@@ -42,33 +39,30 @@ if (fs.existsSync(config_filename)) {
 
             if (data && data.length > 0) {
                 const filename = voc_filename.split(".")[1].split("/")[2];
-                // const out_path  = "./"+voc_filename.split(".")[1].split("/")[1]+"/yoyo.ttl";
-                const out_path  = "./dist/yoyo.ttl";
-
+                const out_path  = "./dist/"+filename+".ttl";
                 console.log(`${data.length} records found`);
 
-                const base_url = "https://w3id.org/iqb/"+filename;
-                const g = rdflib.graph();
-
+                const g = graph();
+                const base_url1 ="https://w3id.org/iqb/"+filename
+                const base_url = g.sym("https://w3id.org/iqb/"+filename+`/#`);
                 g.add(base_url, RDF('type'), SKOS('ConceptScheme'));
-                g.add(base_url, DCTERMS('title'), filename);
-                g.add(base_url, DCTERMS('creator'), creator);
-
+                g.add(base_url, DCTERMS('title'), literal(filename,'de'));
+                g.add(base_url, DCTERMS('creator'), literal(creator,'de'));
 
                 data.forEach((d: any) => {
-                    const c_url = g.sym(base_url + "/"+`${d.id}`);
+                    const c_url = g.sym(base_url1+`/`+`${d.id}`);
                     g.add(c_url, RDF('type'), SKOS('Concept') );
-                    g.add(c_url, SKOS('preLabel'), literal(`${d.title}`));
-                    g.add(c_url, SKOS('notation'), literal(`${d.notation}`));
-                    g.add(base_url, SKOS('hasTopConcept'), c_url);
-                    g.add(c_url, SKOS('topConceptOf'), base_url);
+                    g.add(c_url, SKOS('inScheme'), base_url);
+                    g.add(c_url, SKOS('prefLabel'), literal(`${d.title}`,'de'));
                     if (d.description !="")
-                        g.add(c_url, SKOS('description'), literal(`${d.description}`));
+                        g.add(c_url, SKOS('description'), literal(`${d.description}`,'de'));
+                    g.add(base_url, SKOS('hasTopConcept'), c_url);
+                    g.add(c_url, SKOS('notation'), literal(`${d.notation}`));
                 });
 
-                const output = rdflib.serialize(undefined, g,{base: base_url},{contenType: 'text/turtle'});
+                const output = rdflib.serialize(null, g,undefined,'text/turtle');
                 console.log(output);
-                fs.writeFile(out_path, output, {encoding:'utf8'}, () => console.error("Tengo error"));
+                fs.writeFile(out_path, output, {encoding:'utf8'}, () => console.error(""));
 
             } else {
                 console.log(`\x1b[0;31mERROR\x1b[0m File '${voc_filename}' empty`);
