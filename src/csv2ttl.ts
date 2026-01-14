a4fcce849c03c8cf733180c9e2d87aa4cbc9d2c4
Yan Ping Chen Lin <yanpingchenlin@gmail.com>
2026-01-14 16:30:39 +0100

    Refactor

--- src/csv2ttl.ts
+++ src/csv2ttl.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import fs = require('fs');
-import { ConfigFileFactory, VocabularyData } from './config-file.factory';
+import { ConfigData, ConfigFileFactory, VocabularyData } from './config-file.factory';
 import { CsvFactory } from './csv.factory';
-
-require('fs');
 
 let dataFolder = '.';
 if (process.argv[2]) {
@@ -18,227 +16,153 @@ function getNotationDeep(notation: string): number {
   return Math.max(1, notation.split('.').length);
 }
 
+function getPrefLabel(d: any, voc: VocabularyData, numLang: number): string {
+  const labels = [`"${d.title}"@${voc.title[0].lang}`];
+  if (numLang === 2) {
+    labels.push(`"${d.title_en}"@${voc.title[1].lang}`);
+  } else if (numLang > 2) {
+    labels.push(`"${d.title_fr}"@${voc.title[2].lang}`);
+    labels.push(`"${d.title_it}"@${voc.title[3].lang}`);
+    labels.push(`"${d.title_rm}"@${voc.title[4].lang}`);
+    labels.push(`"${d.title_en}"@${voc.title[1].lang}`);
+  }
+  return labels.join(',\n\t');
+}
+
+function getDefinition(d: any, voc: VocabularyData, numLang: number): string {
+  const definitions = [`"${d.description}"@${voc.title[0].lang}`];
+  if (numLang > 2) {
+    if (d.description_fr) definitions.push(`"${d.description_fr}"@${voc.title[2].lang}`);
+    if (d.description_it) definitions.push(`"${d.description_it}"@${voc.title[3].lang}`);
+    if (d.description_rm) definitions.push(`"${d.description_rm}"@${voc.title[4].lang}`);
+  }
+  if (numLang > 1 && d.description_en) {
+    definitions.push(`"${d.description_en}"@${voc.title[1].lang}`);
+  }
+  return definitions.join(',\n\t');
+}
+
+function getConceptSchemeStrings(configData: ConfigData, voc: VocabularyData) {
+  const numLang = configData.title.length;
+  const langIdx = [0, 2, 3, 4, 1].slice(0, numLang);
+
+  const mainTitles = langIdx.map(i => `"${configData.title[i].value} - ${voc.title[i].value}"@${voc.title[i].lang}`
+  ).join(',\n\t');
+
+  const creators = langIdx.map(i => `"${configData.creator}"@${voc.title[i].lang}`
+  ).join(',\n\t');
+
+  let mainDescription = '';
+  if (voc.description[0].value !== '') {
+    mainDescription = voc.description.slice(0, numLang).map(d => `"${d.value}"@${d.lang}`
+    ).join(',\n\t');
+  }
+
+  return { mainTitles, creators, mainDescription };
+}
+
 const configData = ConfigFileFactory.load(dataFolder);
 if (configData) {
-  let outputFolder = '.';
-  if (configData.outDir) {
-    outputFolder = configData.outDir;
-    if (!fs.existsSync(outputFolder)) {
-      fs.mkdirSync(outputFolder);
-    }
+  const outputFolder = configData.outDir || '.';
+  if (configData.outDir && !fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
   }
+
   const fileList: { [name: string]: string } = {};
   fs.readdirSync(dataFolder).forEach((file: string) => {
     fileList[file.toUpperCase()] = `${dataFolder}/${file}`;
   });
-  const csvDelimiter = configData.csv_delimiter || ';';
 
-  const stoutBase:string = '@prefix dct: <http://purl.org/dc/terms/>.\n' +
-        '@prefix skos: <http://www.w3.org/2004/02/skos/core#>. \n' +
-        '@prefix dc: <http://purl.org/dc/elements/1.1/>.\n' +
-        `@prefix n0: <${configData.base}`;
+  const csvDelimiter = configData.csv_delimiter || ';';
+  const stoutBase = '@prefix dct: <http://purl.org/dc/terms/>.\n' +
+      '@prefix skos: <http://www.w3.org/2004/02/skos/core#>. \n' +
+      '@prefix dc: <http://purl.org/dc/elements/1.1/>.\n' +
+      `@prefix n0: <${configData.base}`;
 
   configData.vocabularies.forEach((voc: VocabularyData) => {
-    if (configData) {
-      const vocFilename = fileList[ConfigFileFactory.getFilenameSource(voc).toUpperCase()];
-      const outPath = `${outputFolder}/${ConfigFileFactory.getFilenameTarget(voc)}`;
-      const header = `${stoutBase}/${voc.id}/>. \n\n`;
-      const baseUrl = 'n0:';
-      let footer = '';
-      // check if there is more than one lang
+    const vocFilename = fileList[ConfigFileFactory.getFilenameSource(voc).toUpperCase()];
+    if (!vocFilename) {
+      console.log(`\x1b[0;33mWARNING\x1b[0m file '${dataFolder}/${voc.id}.csv' not found - ignore`);
+      return;
+    }
 
+    const outPath = `${outputFolder}/${ConfigFileFactory.getFilenameTarget(voc)}`;
+    const baseUrl = 'n0:';
+    const { mainTitles, creators, mainDescription } = getConceptSchemeStrings(configData, voc);
+    const license = `${configData.license};\n`;
+
+    let footer = `${baseUrl}\n` +
+        '\ta skos:ConceptScheme;\n' +
+        `\tdct:creator ${creators};\n` +
+        `\tdct:title ${mainTitles};\n${
+          mainDescription ? `\tdct:description ${mainDescription};\n` : `\tdc:title ${mainTitles};\n\tdc:description ${mainTitles};\n`
+        }\tdct:license ${license}` +
+        '\tskos:hasTopConcept';
+
+    const data = CsvFactory.load(vocFilename, csvDelimiter, false);
+    if (data && data.length > 0) {
+      console.log(`Processing '${vocFilename}': ${data.length} records found`);
+      let stout = `${stoutBase}/${voc.id}/>. \n\n`;
+
+      const urlStack: string[] = [baseUrl];
+      let nodesStack: string[] = [];
+      const bodyStack: string[] = [];
+      const nodeNodesStack: string[][] = [];
       const numLang = configData.title.length;
-      let mainTitle = '';
-      let creator = '';
 
-      mainTitle = `${mainTitle}"${configData.title[0].value} - ${voc.title[0].value}"@${voc.title[0].lang}`;
-      creator = `${creator}"${configData.creator}"@${voc.title[0].lang}`;
-      if (numLang === 5) {
-        mainTitle = `${mainTitle},\n\t"${configData.title[2].value} - ${voc.title[2].value}"@${voc.title[2].lang}` +
-            `,\n\t"${configData.title[3].value} - ${voc.title[3].value}"@${voc.title[3].lang}` +
-            `,\n\t"${configData.title[4].value} - ${voc.title[4].value}"@${voc.title[4].lang}` +
-            `,\n\t"${configData.title[1].value} - ${voc.title[1].value}"@${voc.title[1].lang};\n`;
-        creator = `${creator},\n\t"${configData.creator}"@${voc.title[2].lang}` +
-                            `,\n\t"${configData.creator}"@${voc.title[3].lang}` +
-                            `,\n\t"${configData.creator}"@${voc.title[4].lang}` +
-                            `,\n\t"${configData.creator}"@${voc.title[1].lang};\n`;
-      } else if (numLang === 2) {
-        mainTitle = `${mainTitle},\n\t"${configData.title[1].value} - ${voc.title[1].value}"@${voc.title[1].lang};\n`;
-        creator = `${creator},\n\t"${configData.creator}"@${voc.title[1].lang};\n`;
-      } else {
-        creator = `${creator};\n`;
-        mainTitle = `${mainTitle};\n`;
-      }
-      if (voc.description[0].value === '') {
-        footer = `${baseUrl}\n` +
-                    '\ta skos:ConceptScheme;\n' +
-                    `\tdct:creator ${creator}` +
-                    `\tdct:title ${mainTitle}` +
-                    `\tdc:title ${mainTitle}` +
-                    `\tdc:description ${mainTitle}` +
-                    '\tdct:license <https://creativecommons.org/publicdomain/zero/1.0/deed.de>;\n' +
-                    '\tskos:hasTopConcept';
-      } else {
-        let mainDescription = '';
-        for (let i = 0; i < numLang - 1; i++) {
-          mainDescription = `${mainDescription}"${voc.description[i].value}"@${voc.description[i].lang},\n\t`;
-        }
-        mainDescription = `${mainDescription}"${voc.description[numLang - 1].value}` +
-                          `"@${voc.description[numLang - 1].lang};\n`;
-        const license = `${configData.license};\n`;
-        footer = `${baseUrl}\n` +
-                    '\ta skos:ConceptScheme;\n' +
-                    `\tdct:creator ${creator}` +
-                    `\tdct:title ${mainTitle}` +
-                    `\tdct:description ${mainDescription}` +
-                    `\tdct:license ${license}` +
-                    '\tskos:hasTopConcept';
-      }
-      let stout = header;
+      for (let i = 0; i < data.length; i++) {
+        const d = data[i];
+        const deep = getNotationDeep(d.notation.toString().trim());
+        const deepNext = (i + 1 < data.length) ? getNotationDeep(data[i + 1].notation.toString().trim()) : 1;
 
-      if (vocFilename) {
-        const data = CsvFactory.load(vocFilename, csvDelimiter, false);
-        if (data && data.length > 0) {
-          console.log(`Processing '${vocFilename}': ${data.length} records found`);
-          // initiation of variables for loop:
-          const urlStack: string[] = [];
-          let nodesStack: string[] = [];
-          const bodyStack: string[] = [];
-          const nodeNodesStack: string[][] = [];
-          let oldUrl = baseUrl;
-          const num = data.length;
+        const oldUrl = urlStack[urlStack.length - 1];
+        const newUrl = `n0:${d.id}`;
+        const prefLabel = getPrefLabel(d, voc, numLang);
+        const desc = getDefinition(d, voc, numLang);
 
-          urlStack.push(baseUrl);
-          for (let i = 0; i < num; i++) {
-            const d = data[i];
-            const deep = getNotationDeep(d.notation.toString().trim());
-            let deepNext = deep;
-            // check the deep of the next record
-            if ((i + 1) < num) {
-              const s = data[i + 1];
-              deepNext = getNotationDeep(s.notation.toString().trim());
-            } else {
-              deepNext = 1;
-            }
-            oldUrl = urlStack[urlStack.length - 1];
-            const newUrl = `n0:${d.id}`;
-            let prefLabel = '';
-            prefLabel = `${prefLabel}"${d.title}"@${voc.title[0].lang}`;
-            if (numLang === 2) {
-              prefLabel = `${prefLabel},\n\t"${d.title_en}"@${voc.title[1].lang}`;
-            } else if (numLang > 2) {
-              prefLabel = `${prefLabel},\n\t"${d.title_fr}"@${voc.title[2].lang}` +
-                  `,\n\t"${d.title_it}"@${voc.title[3].lang}` +
-                  `,\n\t"${d.title_rm}"@${voc.title[4].lang}` +
-                  `,\n\t"${d.title_en}"@${voc.title[1].lang}`;
-            }
-            let desc = '';
-            desc = `${desc}"${d.description}"@${voc.title[0].lang}`;
-            if (d.description_fr !== '' && numLang > 2) {
-              desc = `${desc},\n\t"${d.description_fr}"@${voc.title[2].lang}`;
-            }
-            if (d.description_it !== '' && numLang > 2) {
-              desc = `${desc},\n\t"${d.description_it}"@${voc.title[3].lang}`;
-            }
-            if (d.description_rm !== '' && numLang > 2) {
-              desc = `${desc},\n\t"${d.description_rm}"@${voc.title[4].lang}`;
-            }
-            if (d.description_en !== '' && numLang > 1) {
-              desc = `${desc},\n\t"${d.description_en}"@${voc.title[1].lang}`;
-            }
-            if (deepNext === deep || deepNext < deep) {
-              let body = `${newUrl}\n`;
+        let body = `${newUrl}\n\ta skos:Concept;\n` +
+            `\tskos:inScheme ${baseUrl};\n` +
+            `\tskos:notation "${d.notation}";\n${
+              oldUrl === baseUrl ? `\tskos:topConceptOf ${oldUrl};\n` : `\tskos:broader ${oldUrl};\n`
+            }\tskos:prefLabel ${prefLabel}`;
 
-              if (oldUrl === baseUrl) {
-                body = `${body}\t a skos:Concept;\n` +
-                                    `\tskos:inScheme ${baseUrl};\n` +
-                                    `\tskos:notation "${d.notation}";\n` +
-                                    `\tskos:topConceptOf ${oldUrl};\n` +
-                                    `\tskos:prefLabel ${prefLabel}`;
-              } else {
-                body = `${body}\t a skos:Concept;\n` +
-                                    `\tskos:inScheme ${baseUrl};\n` +
-                                    `\tskos:notation "${d.notation}";\n` +
-                                    `\tskos:broader ${oldUrl};\n` +
-                                    `\tskos:prefLabel ${prefLabel}`;
+        if (deepNext <= deep) {
+          if (d.description !== '') {
+            body += `; \n\tskos:definition ${desc}.\n`;
+          } else body += '.\n';
+          nodesStack.push(newUrl);
+          stout += body;
+
+          if (deepNext < deep) {
+            nodeNodesStack.push(nodesStack);
+            let dif = deep - deepNext;
+            while (dif > 0) {
+              urlStack.pop();
+              let oldBody = bodyStack.pop();
+              const tempNodesStack = nodeNodesStack.pop();
+              if (tempNodesStack) {
+                oldBody += `;\n\tskos:narrower ${tempNodesStack.join(',\n\t\t')}.`;
+                stout += `${oldBody}\n`;
               }
-
-              if (d.description !== '') {
-                body = `${body}; \n\tskos:definition ${desc}.\n`;
-              } else body = `${body}.\n`;
-              nodesStack.push(newUrl);
-              stout = `${stout}${body}`;
-
-              // In this case I have to write out the father with the nodesStack
-              if (deepNext < deep) {
-                nodeNodesStack.push(nodesStack);
-                let dif = deep - deepNext;
-                while (dif > 0) {
-                  urlStack.pop();
-                  let oldBody = bodyStack.pop();
-                  const tempNodesStack = nodeNodesStack.pop();
-                  if (tempNodesStack !== undefined) {
-                    oldBody = `${oldBody};\n` +
-                                            '\tskos:narrower ';
-                    tempNodesStack.forEach(node => {
-                      oldBody = `${oldBody}\n\t\t${node},`;
-                    });
-                    oldBody = oldBody?.replace(/.$/, '.');
-                    stout = `${stout}${oldBody}\n`;
-                  }
-                  dif -= 1;
-                }
-                nodesStack = (nodeNodesStack.pop() as string[]);
-              }
-            } else { /* If the deep of the next more than me. Actions:
-                                    1. Store body of myself
-                                    2. Store the actual nodesStack at nodeNodesStack
-                                    3. Store the actual father
-                                    4. Empty the nodesStack
-                                    5. Store the actual deep
-                                */
-              let body = `${newUrl}\n`;
-
-              if (oldUrl === baseUrl) {
-                body = `${body}\t a skos:Concept;\n` +
-                                    `\tskos:inScheme ${oldUrl};\n` +
-                                    `\tskos:notation "${d.notation}";\n` +
-                                    `\tskos:topConceptOf ${oldUrl};\n` +
-                                    `\tskos:prefLabel ${prefLabel}`;
-              } else {
-                body = `${body}\t a skos:Concept;\n` +
-                                    `\tskos:inScheme ${baseUrl};\n` +
-                                    `\tskos:notation "${d.notation}";\n` +
-                                    `\tskos:broader ${oldUrl};\n` +
-                                    `\tskos:prefLabel ${prefLabel}`;
-              }
-
-              if (d.description !== '') {
-                body = `${body}; \n\tskos:definition ${desc}`;
-              }
-
-              bodyStack.push(body);
-              nodesStack.push(newUrl);
-              nodeNodesStack.push(nodesStack);
-              nodesStack = [];
-              urlStack.push(newUrl);
+              dif -= 1;
             }
+            nodesStack = nodeNodesStack.pop() as string[];
           }
-
-          nodesStack.forEach(node => {
-            footer = `${footer}\n\t\t${node},`;
-          });
-
-          footer = footer.replace(/.$/, '.');
-          stout = `${stout}${footer}`;
-
-          fs.writeFileSync(outPath, stout, { encoding: 'utf8' });
         } else {
-          console.log(`\x1b[0;33mWARNING\x1b[0m Errors in file '${vocFilename}' - ignore`);
+          if (d.description !== '') body += `; \n\tskos:definition ${desc}`;
+          bodyStack.push(body);
+          nodesStack.push(newUrl);
+          nodeNodesStack.push(nodesStack);
+          nodesStack = [];
+          urlStack.push(newUrl);
         }
-      } else {
-        console.log(`\x1b[0;33mWARNING\x1b[0m file '${dataFolder}/${voc.id}.csv' not found - ignore`);
       }
+
+      footer += `\n\t\t${nodesStack.join(',\n\t\t')}.`;
+      stout += footer;
+      fs.writeFileSync(outPath, stout, { encoding: 'utf8' });
+    } else {
+      console.log(`\x1b[0;33mWARNING\x1b[0m Errors in file '${vocFilename}' - ignore`);
     }
   });
 }
